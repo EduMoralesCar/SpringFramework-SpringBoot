0691488c5f329d1f699647f15fb1232a8a3f86a8
Edomocar <edumoralescarlos@gmail.com>
2026-04-14 18:27:59 -0500

    test: agregar clases de test generadas por Spring Boot

---
+++ Universidad/minka/src/test/java/com/utp/minka/MinkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.minka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
