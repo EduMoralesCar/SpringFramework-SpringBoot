53a6ab090af25fbfa03a10cee54d7a52c462864c
Edomocar <edumoralescarlos@gmail.com>
2026-04-14 18:27:46 -0500

    chore(config): configuracion inicial del proyecto Spring Boot y properties

---
+++ Universidad/minka/src/main/java/com/utp/minka/MinkaApplication.java
@@ -0,0 +1,13 @@
+package com.utp.minka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinkaApplication.class, args);
+	}
+
+}
