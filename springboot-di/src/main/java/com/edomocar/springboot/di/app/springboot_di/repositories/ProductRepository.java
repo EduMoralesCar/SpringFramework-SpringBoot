1e1fd4bb66128cc400c7bb4f8144ed026c49b875
Edomocar <edumoralescarlos@gmail.com>
2026-05-06 20:08:17 -0500

    refactor(ProductRepository): cambiar clase a interfaz y eliminar implementación de métodos

--- springboot-di/src/main/java/com/edomocar/springboot/di/app/springboot_di/repositories/ProductRepository.java
+++ springboot-di/src/main/java/com/edomocar/springboot/di/app/springboot_di/repositories/ProductRepository.java
@@ -2,30 +2,11 @@ package com.edomocar.springboot.di.app.springboot_di.repositories;
 
 import java.util.List;
 import com.edomocar.springboot.di.app.springboot_di.models.Product;
-import java.util.Arrays;
 
-public class ProductRepository {
+public interface ProductRepository {
 
-    private List<Product> data;
+    List<Product> findAll();
 
-    public ProductRepository() {
-        this.data = Arrays.asList(
-            new Product(1L, "Laptop", 999.99),
-            new Product(2L, "Smartphone", 499.99),
-            new Product(3L, "Tablet", 299.99)
-        );
-    }
+    Product findById(Long id);
 
-    // Retorna la lista de productos
-    public List<Product> findAll() {
-        return data;
-    }
-
-    // Retorna un producto por su ID
-    public Product findById(Long id) {
-        return data.stream()
-                   .filter(product -> product.getId().equals(id))
-                   .findFirst()
-                   .orElse(null);
-    }
 }
