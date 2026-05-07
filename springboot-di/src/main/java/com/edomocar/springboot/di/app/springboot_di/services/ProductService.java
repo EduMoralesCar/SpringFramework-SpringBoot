2679b68f6b4d412368bb08cadf57786094ef7d35
Edomocar <edumoralescarlos@gmail.com>
2026-05-06 20:08:28 -0500

    refactor(ProductService): cambiar clase a interfaz y eliminar implementación de métodos

--- springboot-di/src/main/java/com/edomocar/springboot/di/app/springboot_di/services/ProductService.java
+++ springboot-di/src/main/java/com/edomocar/springboot/di/app/springboot_di/services/ProductService.java
@@ -1,24 +1,11 @@
 package com.edomocar.springboot.di.app.springboot_di.services;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import com.edomocar.springboot.di.app.springboot_di.repositories.ProductRepository;
 import com.edomocar.springboot.di.app.springboot_di.models.Product;
 
-public class ProductService {
-    private ProductRepository repository = new ProductRepository();
+public interface ProductService {
+    
+    List<Product> findAll();
 
-    public List<Product> findAll() {
-        return repository.findAll().stream().map(p -> {
-            Double precio = p.getPrecio() * 1.25d; // Aplicar un aumento del 25% al precio
-            Product newProduct = new Product(p.getId(), p.getNombre(), precio.longValue()); // Crear un nuevo producto con el precio actualizado
-            
-            return newProduct;
-        }).collect(Collectors.toList());
-    }
-
-    public Product findById(Long id) {
-        return repository.findById(id);
-    }
-
+    Product findById(Long id);
 }
