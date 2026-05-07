34eea57e3ebc3e38b9e112c1d29fbe6d9adc7b8e
Edomocar <edumoralescarlos@gmail.com>
2026-05-06 20:08:23 -0500

    feat(ProductRepositoryFoo): agregar implementación de ProductRepository para manejo de productos

---
+++ springboot-di/src/main/java/com/edomocar/springboot/di/app/springboot_di/repositories/ProductRepositoryFoo.java
@@ -0,0 +1,21 @@
+package com.edomocar.springboot.di.app.springboot_di.repositories;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.edomocar.springboot.di.app.springboot_di.models.Product;
+
+@Repository("productoFoo")
+public class ProductRepositoryFoo implements ProductRepository {
+    @Override
+    public List<Product> findAll() {
+        return Collections.singletonList(new Product(1L, "Monitor Asus 27", 600L));
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return new Product(id, "Monitor Asus 27", 600L);
+    }
+}
