74ab50adaae8f05c3d3194a4a3ccbc4c7c29604a
Edomocar <edumoralescarlos@gmail.com>
2026-05-13 18:46:55 -0500

    fix(test): corregir ruta del contexto de SpringBootTest para la aplicacion

---
+++ Universidad/ProyectoModelo/src/test/java/com/UTP/ProyectoModelo/TpIntegradoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.UTP.ProyectoModelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.UTP.TpIntegrado.market.TpIntegradoApplication;
+
+@SpringBootTest(classes = TpIntegradoApplication.class)
+class TpIntegradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
